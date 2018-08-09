1e9ddf501eb495a463f9f51c5df7c79269167dfe
kyle-barth <kylebarth@me.com>
2018-08-09 14:00:42 +0100

    Lecture 27 - Nesting Components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -23,10 +36,18 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <ol>
-                    <li>What should I do?</li>
-                    <li>What should I do?</li>
-                </ol>
+                Options component here
+                <Option />
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                Option
             </div>
         )
     }
@@ -42,13 +63,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
