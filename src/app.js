527632ee015bb6458911abcbdd49908d35a51974
kyle-barth <kylebarth@me.com>
2018-08-09 13:51:36 +0100

    Lecture 26 - Creating a React Component

--- src/app.js
+++ src/app.js
@@ -1,70 +1,54 @@
-console.log('App.js is running!');
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render();
-    };
-};
-
-const onRemoveAll = () => {
-    app.options = [];
-    render();
-};
-
-const onMakeDecision = () => {
-    const randNum = Math.floor(Math.random() * app.options.length)
-    const option = app.options[ randNum ]
-    alert(option);
-};
-
-const appRoot = document.getElementById('app');
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>{ app.title }</h1>
-            { app.subtitle && <p>{ app.subtitle }</p> }
-    
-            <p>{ app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
-            <button disabled={ app.options.length === 0 } onClick={ onMakeDecision }>What should I do?</button>
-            <button onClick={ onRemoveAll }>Remove All</button>
-            <ol>
-                {
-                    app.options.map((option) => {
-                        return <li key={ option }>Option: { option }</li>;
-                    })
-                }
-            </ol>
-            <form onSubmit={ onFormSubmit }>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-
-    ReactDOM.render(template, appRoot);
-};
-
-const user = {
-    name: 'Kyle Barth',
-    age: 21,
-    location: 'London'
-};
-
-function getLocation(location) {
-    if (location) {
-        return <p>Location: { location }</p>;
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
     }
-};
+}
 
-render();
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    <li>What should I do?</li>
+                    <li>What should I do?</li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Add Option</button>
+            </div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
