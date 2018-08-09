8aceaed933df7be0be713da09f61599fe1e823b0
kyle-barth <kylebarth@me.com>
2018-08-09 16:20:10 +0100

    Lecture 35 - Adding State to VisibilityToggle

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,22 +1,52 @@
-const appRoot = document.getElementById('app')
+class Visibility extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleToggle = this.handleToggle.bind(this)
+        this.state = {
+            visibility: false
+        }
+    }
 
-let visibility = false
-
-const onToggle = () => {
-    visibility = !visibility
-    render()
+    handleToggle(e) {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={ this.handleToggle }>Show Details</button>
+                { this.state.visibility && <p>Now you see me</p> } 
+            </div>
+        )
+    }
 }
 
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={ onToggle }>Show Details</button>
-            { visibility && <p>Now you see me</p> } 
-        </div>
-    )
+ReactDOM.render(<Visibility />, document.getElementById('app'));
 
-    ReactDOM.render(template, appRoot)
-}
+// const appRoot = document.getElementById('app')
 
-render()
+// let visibility = false
+
+// const onToggle = () => {
+//     visibility = !visibility
+//     render()
+// }
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={ onToggle }>Show Details</button>
+//             { visibility && <p>Now you see me</p> } 
+//         </div>
+//     )
+
+//     ReactDOM.render(template, appRoot)
+// }
+
+// render()
