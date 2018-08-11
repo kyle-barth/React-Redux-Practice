c0cbe1cfe272deb046ea6a65ed2289d60da375f0
kyle-barth <kylebarth@me.com>
2018-08-11 16:04:31 +0100

    Lecture 55 - One Component per File

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-import validator from 'validator'
-
-console.log(validator.isEmail('foo@bar.com'), validator.isEmail('foo'))
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const template = <p>This is JSX from webpack</p>
-ReactDOM.render(template, document.getElementById('app'))
+// App
+import IndecisionApp from './components/IndecisionApp'
+
+ReactDOM.render(<IndecisionApp options={[]} />, document.getElementById('app'))
--- src/person.js
+++
@@ -1,5 +0,0 @@
-const isAdult = (age) => age >= 18
-const canDrink = (age) => age >= 18
-const isSeniorCitizen = (age) => age >= 65 // exported as default, so name doesn't matter in app.js 
-
-export {isAdult, canDrink, isSeniorCitizen as default}
