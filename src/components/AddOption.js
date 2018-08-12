b1a16365cf89a6a9956b2bd494775f05b9ccc90b
kyle-barth <kylebarth@me.com>
2018-08-12 21:08:38 +0100

    Lecuture 57 - Webpack Dev Server

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -13,8 +13,6 @@ export default class AddOption extends React.Component {
         e.preventDefault()
         const option = e.target.elements.option.value.trim()
         const error = this.props.handleAddOption(option)
-        
-        console.log('testing')
 
         this.setState(() => ({error}))        
         // Same as...
