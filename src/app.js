2bc4e379b4b98361b6033d4bac3e95b9ea280768
kyle-barth <kylebarth@me.com>
2018-08-13 22:06:32 +0100

    Lecture 64 - Setting up Webpack with SCSS

--- src/app.js
+++ src/app.js
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom'
 // App
 import IndecisionApp from './components/IndecisionApp'
 
+// Styling
+import './styles/styles.scss'
+
 ReactDOM.render(<IndecisionApp options={[]} />, document.getElementById('app'))
