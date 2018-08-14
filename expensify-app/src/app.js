7035c3fbc8e50a7507a548c57464d91c0874f874
kyle-barth <kylebarth@me.com>
2018-08-14 09:43:25 +0100

    Lecture 76 - Setting Up Budget App

---
+++ expensify-app/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// App
+
+
+// Styling
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
