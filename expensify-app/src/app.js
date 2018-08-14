5815c37775bdc9499be42da04db314bd9ecaca77
kyle-barth <kylebarth@me.com>
2018-08-14 11:01:50 +0100

    Lecture 77 - React-Router 101

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-// App
-
+import {BrowserRouter, Route} from 'react-router-dom'
 
 // Styling
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'))
+// App
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
