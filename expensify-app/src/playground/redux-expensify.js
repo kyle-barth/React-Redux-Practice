c5ac32560dcac61693da6a2ffaab1fb75b6495f8
kyle-barth <kylebarth@me.com>
2018-08-22 16:50:13 +0100

    Lecture 92.5 - Spread Operators in Reducers (10:05)

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,6 +1,25 @@
 import { createStore, combineReducers } from 'redux'
+import uuid from 'uuid'
 
 // ADD_EXPENSE
+const addExpense = (
+    { 
+        description  = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+})
+
 // REMOVE_EXPENSE
 // EDIT_EXPENSE 
 // SET_TEXT_FILTER
@@ -14,6 +33,8 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return state.concat(action.expense)
         default:
             return state
     }
@@ -41,7 +62,11 @@ const store = createStore(
     })
 )
 
-console.log(store.getState())
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
 
 const demoState = {
     expenses: [{
